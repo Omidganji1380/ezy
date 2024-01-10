3f11e67abec25de319824803a918fc694fdfd6c3
Omidganji1380 <30316993+Omidganji1380@users.noreply.github.com>
2024-01-10 13:43:15 +0330

    addEnToDB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,12 @@ return new class extends Migration {
             $table->string('avatar')->nullable();
             $table->string('email')->unique()->nullable();
             $table->string('password')->nullable();
-            $table->string('phone')->unique();
+            $table->string('phone')->unique()->nullable();
             $table->string('postCode')->nullable();
             $table->text('address')->nullable();
             $table->tinyInteger('role')->default(1);
             $table->string('name_en')->nullable();
-            $table->string('phone_en')->unique();
+            $table->string('phone_en')->unique()->nullable();
             $table->string('postCode_en')->nullable();
             $table->text('address_en')->nullable();
             $table->rememberToken();
