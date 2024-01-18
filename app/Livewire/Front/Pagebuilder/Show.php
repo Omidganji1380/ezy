6f60d349acddcee9114d2a20b61d3de468f4d09e
Omidganji1380 <30316993+Omidganji1380@users.noreply.github.com>
2024-01-18 12:02:33 +0330

    showProfile

--- app/Livewire/Front/Pagebuilder/Show.php
+++ app/Livewire/Front/Pagebuilder/Show.php
@@ -2,14 +2,54 @@
 
 namespace App\Livewire\Front\Pagebuilder;
 
+use App\Models\Block;
+use App\Models\BlockPbOption;
+use App\Models\Profile;
 use Livewire\Component;
 
 class Show extends Component
 {
-    public function mount()
+    public $profile;
+    public $blocks;
+    public function mount($link)
     {
+        $this->profile=Profile::query()->where('link',$link)->first();
+        if (!$this->profile){
+            abort(404);
+        }
+        $this->blocks = $this->profile->block;
+//        dd($this->profile);
     }
-
+    public function getBlockItemsBorder(Block $block)
+    {
+        $border = $block->blockOption->blockBorder;
+        if ($border == 0) {
+            $border = '0 !important';
+        }
+        elseif ($border == 1) {
+            $border = '0.25rem !important';
+        }
+        elseif ($border == 2) {
+            $border = '10px !important';
+        }
+        elseif ($border == 3) {
+            $border = '100px !important';
+        }
+        return $border;
+    }
+    public function getIconPaths()
+    {
+        for ($ii = 1; $ii <= 50; $ii++) {
+            echo '<span class="path' . $ii . '"></span >';
+        }
+    }
+    public function getBlockTitle($blockPbOption)
+    {
+//        dd($blockPbOption);
+        $a = BlockPbOption::query()->where(['pbOption_id' => $blockPbOption->pbOption_id, 'id' => $blockPbOption->id, 'block_id' => $blockPbOption->block_id])->first();
+//        dd($a);
+        return $a->title;
+    }
     public function render()
     {
         return view('livewire.front.pagebuilder.show')->layout('components.layouts.pageBuilder.app');
--- resources/views/livewire/front/pagebuilder/show.blade.php
+++ resources/views/livewire/front/pagebuilder/show.blade.php
@@ -1,3 +1,231 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    @push('css')
+        <style>
+            .selected {
+                border-bottom: 2px solid blue;
+            }
+
+            .btn.b1:focus, .btn.b2:focus {
+                box-shadow: unset !important;
+            }
+
+            .btnNoFocus {
+                box-shadow: unset !important;
+            }
+        </style>
+        <style>
+            .userImageDiv {
+                width: 22%;
+                height: 37%;
+                left: 0;
+                right: 0;
+                bottom: 15%;
+            }
+
+            .backgroundImage {
+                max-height: 150px;
+                color: white;
+                position: absolute;
+                object-fit: cover;
+                object-position: center center;
+                z-index: 1;
+                width: 100%;
+                border-radius: 0px;
+
+            }
+
+            .userImage {
+                width: 100px;
+                height: 100px;
+                color: white;
+                z-index: 5;
+                border: 1px solid grey;
+                object-fit: contain;
+                border-radius: 100%;
+                margin-top: 95px;
+            }
+
+            .userDiv {
+                width: 100%;
+                display: flex;
+                flex-direction: column;
+                -webkit-box-align: center;
+                align-items: center;
+                position: relative;
+                z-index: 5;
+                margin-bottom: 22px;
+                font-family: iransans;
+            }
+
+            .user-info-data {
+                display: flex;
+                flex-direction: column;
+                -webkit-box-pack: start;
+                justify-content: flex-start;
+                -webkit-box-align: center;
+                align-items: center;
+                margin-left: 0px;
+                z-index: 5;
+                margin-bottom: 8px;
+                width: 100%;
+
+            }
+
+            .MuiTypography-root.title {
+                font-weight: bold;
+                font-size: 18px;
+                margin-top: 5px;
+            }
+
+            .MuiTypography-root {
+                margin: 0px;
+                font-weight: 400;
+                line-height: 1.5;
+                font-size: 15px;
+                color: inherit;
+                font-family: inherit;
+                padding: 0px 8px;
+                z-index: 5;
+                white-space: break-spaces;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                text-align: center;
+                width: 100%;
+                unicode-bidi: plaintext;
+                overflow-wrap: break-word;
+
+            }
+
+            .col-12.fixed-bottom.mx-auto {
+                border-radius: 20px 20px 0 0;
+                background-color: rgba(32, 32, 32, .6);
+                max-width: 600px;
+                backdrop-filter: blur(5px)
+            }
+
+            .row.p-2.justify-content-end {
+                background: rgba(0, 0, 0, 0.15);
+                border-radius: 8px;
+                backdrop-filter: blur(5px)
+            }
+        </style>
+    @endpush
+
+        <div style="max-width: 600px" class="container">
+            <div class="row">
+                {{--<div class="col-12" style="border-radius: 20px;box-shadow: rgba(0,0,0,0.2) 0 0 20px;">
+                    <div class="row p-2 justify-content-end flex-nowrap">
+                        <div class="col-auto px-1 align-self-center">
+                            ezy.company/{{$profile->link}}
+                        </div>
+                        <div class="col-auto px-1 align-self-center">|</div>
+                        <div class="col-auto px-1">
+                            <button class="btn p-1 text-info">
+                                <i class="icofont-share"></i>
+                            </button>
+                        </div>
+                        <div class="col-auto px-1">
+                            <button class="btn p-1 text-info">
+                                <i class="icofont-ui-copy"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>--}}
+                <div class="col-12 my-3">
+{{--                    <div class="row p-2 flex-nowrap">--}}
+{{--                        <div class="col-11">--}}
+                            <div class="userDiv">
+                                <img src="{{asset('storage/pb/profiles/profile-'.$profile->id.'/'.$profile->bg_img)}}"
+                                     alt="" class="backgroundImage"
+                                     style="border-radius: {{$profile->bg_border==100?'50% / 0 0 100% 100%':''}}">
+                                <img
+                                    src="{{asset($profile->img?'storage/pb/profiles/profile-'.$profile->id.'/'.$profile->img:'userGray.png')}}"
+                                    alt="" style="border-radius: {{$profile->img_border}}%"
+                                    class="userImage">
+                                <div class="user-info-data">
+                                    <p class="MuiTypography-root title">{{$profile->title}}</p>
+                                    <p class="MuiTypography-root">{!! $profile->subtitle !!}</p>
+                                </div>
+{{--                            </div>--}}
+{{--                        </div>--}}
+                    </div>
+                </div>
+{{--                <div id="sortable" style="cursor: grab;margin-bottom: 6rem !important;" wire:ignore.self>--}}
+                    @foreach($blocks as $block)
+                        <div class="col-12 my-3">
+{{--                            <div class="row p-2 flex-nowrap">--}}
+{{--                                <div class="col-11">--}}
+                                    <div class="row justify-content-center">
+                                        <div class="col-12 text-center">
+                                            <p class="text-center">{{$block->blockOption->blockTitle}}</p>
+                                        </div>
+                                        @foreach($block->pbOption()->get() as $option)
+                                            <div class="{{$block->blockOption->blockWidth=='full'?'col-12':($block->blockOption->blockWidth=='half'?'col-6':($block->blockOption->blockWidth=='compress'?'col-auto':''))}} text-center p-1">
+                                                <button dir="rtl"
+                                                        class="btn border-info w-100 overflow-hidden text-truncate px-1"
+                                                        style="border-radius: {{$this->getBlockItemsBorder($block)}};">
+                                                    <div class="row justify-content-center flex-nowrap">
+                                                        <div class="col-auto {{$block->blockOption->blockWidth!='compress'?'ps-0':''}}">
+                                                            <i class="{{$option->icon}} text-info mx-2 align-middle iii"
+                                                               style="font-size: 25px !important;">
+                                                                {!! $this->getIconPaths() !!}
+                                                            </i>
+                                                        </div>
+                                                        @if($block->blockOption->blockWidth!='compress')
+                                                            <div
+                                                                class="col-auto pe-0">{{$this->getBlockTitle($option->pivot)}}</div>
+                                                        @endif
+                                                    </div>
+                                                </button>
+                                            </div>
+                                        @endforeach
+                                    </div>
+{{--                                </div>--}}
+{{--                            </div>--}}
+                        </div>
+                    @endforeach
+{{--                </div>--}}
+
+            </div>
+        </div>
+
+    @push('js')
+
+        <script>
+            $(window).ready(function () {
+                $('i').addClass('fs-5')
+                // $('button').addClass('fs-5')
+                // setInterval(function () {
+                //     var icon = $('.ez');
+                //
+                //     for (var i = 0; i < icon.length; i++) {
+                //         var a       = icon[i];
+                //         a.innerHTML = null
+                //         for (var ii = 1; ii <= 50; ii++) {
+                //             a.innerHTML += "<span class='path" + ii + "'></span>"
+                //         }
+                //     }
+                // }, 2000)
+            })
+            $(document).ready(function () {
+
+                let button1 = $('.b1');
+                let button2 = $('.b2');
+
+                button1.on('click', function () {
+                    rc()
+                    button1.addClass('selected')
+                })
+                button2.on('click', function () {
+                    rc()
+                    button2.addClass('selected')
+                })
+
+                function rc() {
+                    button1.removeClass('selected')
+                    button2.removeClass('selected')
+                }
+            })
+        </script>
+    @endpush
 </div>
