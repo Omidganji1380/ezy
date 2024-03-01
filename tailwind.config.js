4f7168fafabb2ae6b4bd5075afa9d1a9aae1b851
Omidganji1380 <30316993+Omidganji1380@users.noreply.github.com>
2024-03-01 19:37:14 +0330

    the first day of view of vue

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  purge: ['./index.html', './src/assets/tailwind/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const {defineConfig} = require('@vue/cli-service')
+const webpack        = require('webpack');
+
+module.exports = defineConfig({
+                                  transpileDependencies: true,
+                                  chainWebpack         : config => {
+                                      config.module
+                                            .rule('vue')
+                                            .use('vue-loader')
+                                            .tap(options => {
+                                                options.compilerOptions = {
+                                                    ...options.compilerOptions,
+                                                    isCustomElement: tag => tag.startsWith('ion-')
+                                                }
+                                                return options
+                                            })
+                                  },
+                                  configureWebpack     : {
+                                      plugins: [
+                                          new webpack.DefinePlugin({
+                                                                       // Vue CLI is in maintenance mode, and probably won't merge my PR to fix this in their tooling
+                                                                       // https://github.com/vuejs/vue-cli/pull/7443
+                                                                       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+                                                                   })
+                                      ],
+                                  },
+                              })
