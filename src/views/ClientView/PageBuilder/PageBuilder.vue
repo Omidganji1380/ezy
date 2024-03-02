d1435965c4431f0ec2c5f19a0d11769cd15ce889
Omidganji1380 <30316993+Omidganji1380@users.noreply.github.com>
2024-03-02 18:31:47 +0330

    start dashboard

--- src/views/ClientView/PageBuilder/PageBuilder.vue
+++ src/views/ClientView/PageBuilder/PageBuilder.vue
@@ -6,18 +6,25 @@
     <div class="section mt-3 mb-3">
       <div class="row">
         <div class="col-12 col-md-6 col-lg-4 mb-2">
-          <div class="card">
-            <img src="assets/img/sample/photo/wide4.jpg" class="card-img-top" alt="image">
+          <div class="card px-4">
+            <div class="card-header">
+              <div class="bg-gray-100 mx-0 overflow-hidden flex-nowrap px-2 py-2 rounded row">
+                <i class="self-center col-auto px-1 fa fa-copy"></i>
+                <i class="self-center col-auto px-1 fa fa-share-alt"></i>
+                <span class="self-center col-auto px-0">|</span>
+                <span class="self-center px-1 col-auto text-truncate">ezy.company/omidganji1380</span>
+              </div>
+            </div>
             <div class="card-body">
-              <h6 class="card-subtitle">Discover</h6>
-              <h5 class="card-title">Components</h5>
-              <p class="card-text">
-                Reusable components designed for the mobile interface and ready to use.
-              </p>
-              <a href="app-components.html" class="btn btn-primary">
-                <ion-icon name="cube-outline"></ion-icon>
-                Preview
-              </a>
+              <div class="rounded border-1 row justify-content-end p-4">
+                <div class="col-10 text-right">
+                  <!--                  <img src="assets/img/PageBuilder/userGray.png" alt="">-->
+                  omid
+                </div>
+                <div class="col-2">
+                  <img src="assets/img/PageBuilder/userGray.png" alt="">
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@
 //   plugins: [],
 // }
 module.exports = {
-  purge: ['./index.html', './src/assets/tailwind/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
