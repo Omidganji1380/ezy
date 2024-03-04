15463953f1c9a4e0f6cdeff48d3447ee2d0853f5
Omidganji1380 <30316993+Omidganji1380@users.noreply.github.com>
2024-03-04 21:13:45 +0330

    refactor index from figma

---
+++ src/components/ClientIncludes/AppLoader.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div id="loader">
+    <div class="spinner-border text-primary" role="status"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AppLoader"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/ClientView/Index.vue
+++ src/views/ClientView/Index.vue
@@ -1,111 +1,47 @@
 <template>
-
-
   <!-- App Capsule -->
-  <div id="appCapsule">
-
-    <div class="header-large-title">
-      <h1 class="title">Discover</h1>
-      <h4 class="subtitle">Welcome to Mobilekit</h4>
-    </div>
-
-    <div class="section mt-3 mb-3">
-      <div class="card">
-        <div class="card-body d-flex justify-content-between align-items-end">
-          <div>
-            <h6 class="card-subtitle">Discover</h6>
-            <h5 class="card-title mb-0 d-flex align-items-center justify-content-between">
-              Dark Mode
-            </h5>
-          </div>
-          <div class="form-check form-switch">
-            <input class="form-check-input dark-mode-switch" type="checkbox" id="darkmodecontent">
-            <label class="form-check-label" for="darkmodecontent"></label>
-          </div>
-
+  <div id="appCapsule" class="container">
+    <img src="assets/img/logo.svg" :class="{'fade':fadedLogo}"
+         class="left-1/2 position-absolute top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div class="intro fade" dir="rtl" :class="{'show':showIntro}">
+      <div class="header-large-title">
+        <p class="title mb-4 max-w-fit text-truncate">به <span class="text-[#009606]">ایزی کانکت</span> خوش اومدی</p>
+        <p class="subtitle mb-3 max-w-fit text-truncate">برای ورود به اپلیکیشن، شماره موبایلت را وارد کن.</p>
+      </div>
+      <div class="section mt-3 mb-3">
+        <input type="text"
+               class="w-100 border-solid border-2 border-[#009606] focus-visible:outline-0 leading-10 text-center rounded-[10px] bg-[#F0FCF3] text-[#009606]">
+        <p class="subtitle mb-3 max-w-fit text-truncate">با ثبت نام و ورود <a href="#" class="text-[#6772E5]">قوانین و مقررات</a> ایزی کانکت را
+          می‌پذیرم.</p>
+        <div class="text-center">
+          <button class="btn text-[#F0FCF3] bg-[#009606]">ثبت نام و ورود</button>
         </div>
+
       </div>
     </div>
-
-    <div class="section mt-3 mb-3">
-      <div class="card">
-        <img src="assets/img/sample/photo/wide4.jpg" class="card-img-top" alt="image">
-        <div class="card-body">
-          <h6 class="card-subtitle">Discover</h6>
-          <h5 class="card-title">Components</h5>
-          <p class="card-text">
-            Reusable components designed for the mobile interface and ready to use.
-          </p>
-          <a href="app-components.html" class="btn btn-primary">
-            <ion-icon name="cube-outline"></ion-icon>
-            Preview
-          </a>
-        </div>
-      </div>
-    </div>
-
-    <div class="section mt-3 mb-3">
-      <div class="card">
-        <img src="assets/img/sample/photo/wide2.jpg" class="card-img-top" alt="image">
-        <div class="card-body">
-          <h6 class="card-subtitle">Discover</h6>
-          <h5 class="card-title">Pages</h5>
-          <p class="card-text">
-            Mobilekit comes with basic pages you may need and use in your projects easily.
-          </p>
-          <a href="app-pages.html" class="btn btn-primary">
-            <ion-icon name="layers-outline"></ion-icon>
-            Preview
-          </a>
-        </div>
-      </div>
-    </div>
-
-
-    <!-- app footer -->
-    <div class="appFooter">
-      <img src="assets/img/logo.png" alt="icon" class="footer-logo mb-2">
-      <div class="footer-title">
-        Copyright © Mobilekit <span class="yearNow"></span>. All Rights Reserved.
-      </div>
-      <div>Mobilekit is PWA ready Mobile UI Kit Template.</div>
-      Great way to start your mobile websites and pwa projects.
-
-      <div class="mt-2">
-        <a href="#" class="btn btn-icon btn-sm btn-facebook">
-          <ion-icon name="logo-facebook"></ion-icon>
-        </a>
-        <a href="#" class="btn btn-icon btn-sm btn-twitter">
-          <ion-icon name="logo-twitter"></ion-icon>
-        </a>
-        <a href="#" class="btn btn-icon btn-sm btn-linkedin">
-          <ion-icon name="logo-linkedin"></ion-icon>
-        </a>
-        <a href="#" class="btn btn-icon btn-sm btn-instagram">
-          <ion-icon name="logo-instagram"></ion-icon>
-        </a>
-        <a href="#" class="btn btn-icon btn-sm btn-whatsapp">
-          <ion-icon name="logo-whatsapp"></ion-icon>
-        </a>
-        <a href="#" class="btn btn-icon btn-sm btn-secondary goTop">
-          <ion-icon name="arrow-up-outline"></ion-icon>
-        </a>
-      </div>
-
-    </div>
-    <!-- * app footer -->
-
   </div>
   <!-- * App Capsule -->
-
-
 </template>
 
 <script>
 export default {
-  name      : 'Index',
-  components: {
-
+  name: 'Index',
+  data() {
+    return {
+      fadedLogo: false,
+      showIntro: false
+    }
   },
+  components: {},
+  methods   : {},
+  mounted() {
+    setTimeout(() => {
+    setTimeout(() => {
+      this.showIntro = true
+    }, 500)
+      this.fadedLogo = true
+    }, 1500)
+  },
+
 }
 </script>
